c60278a93c43facb33e489c09191f5b2a035cf3a
Zahir Khan <curse3216@gmail.com>
2026-08-04 01:43:20 +0530

    feat: implement ticket  creation API

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,12 @@ from fastapi import FastAPI
 from app.core.database import Base, engine
 from app.models.ticket import Ticket
 from app.models.note import Note
+from app.routers.tickets import router as ticket_router
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(
-    title="SupportFlow API",
-    version="1.0.0",
-)
+app = FastAPI(title="SupportFlow API",version="1.0.0")
+app.include_router(ticket_router)
 
 @app.get("/")
 def root():
--- backend/app/services/ticket_service.py
+++ backend/app/services/ticket_service.py
@@ -5,17 +5,23 @@ from app.models.ticket import Ticket
 from app.schemas.ticket import TicketCreate
 
 def create_ticket(db:Session, ticket:TicketCreate):
-    db_ticket=Ticket(
-        ticket_id=f"TKT_{uuid.uuid4().hex[:8].upper()}",
-        customer_name=ticket.customer_name,
-        customer_email=ticket.customer_email,
-        subject=ticket.subject,
-        description=ticket.description,
-        status=TicketStatus.OPEN,
-    )
 
-    db.add(db_ticket)
-    db.commit
-    db.refresh(db_ticket)
+    try:
+        db_ticket=Ticket(
+                ticket_id=f"TKT_{uuid.uuid4().hex[:8].upper()}",
+                customer_name=ticket.customer_name,
+                customer_email=ticket.customer_email,
+                subject=ticket.subject,
+                description=ticket.description,
+                status=TicketStatus.OPEN,
+                )
 
-    return db_ticket
+        db.add(db_ticket)
+        db.commit()
+        db.refresh(db_ticket)
+
+        return db_ticket
+
+    except Exception as e:
+        db.rollback()
+        raise e
